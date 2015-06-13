4a8c4d0454b0b27390ee738a8c1a58b46a47773a
LionNatsu <lionxlion@foxmail.com>
2015-06-13 11:07:03 +0800

    Wrote some comments
    Signed-off-by: LionNatsu <lionxlion@foxmail.com>

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {  // when we get a request to get the root
   res.render('index', { title: 'Hey', message: 'Hello there!'});
+  // render index.jade with a json param
+  console.log('Render to %s', req.ip);
 });
 
-app.use(express.static('public'));
-app.set('view engine', 'jade');
-app.set('views', './views');
+app.use(express.static('public'));  // mount this directory to *web root*
+app.set('view engine', 'jade');     // use *Jade* to render templates
+app.set('views', './views');        // and here is the templates
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function () { // start service
   console.log('Listening at port %s', server.address().port);
 });
