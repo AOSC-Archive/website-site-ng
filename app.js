a3283c8440c221c53c3bab278cd6524da0d43d0d
LionNatsu <lionxlion@foxmail.com>
2015-06-14 00:17:40 +0800

    'Automatic Generator'
    Signed-off-by: LionNatsu <lionxlion@foxmail.com>

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
-var app = express();
+var yaml    = require('js-yaml');
+var fs      = require('fs');
+var app     = express();
 
-app.get('/', function (req, res) {  // when we get a request to get the root
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
-  // render index.jade with a json param
-  console.log('Render to %s', req.ip);
+app.get('/', function (req, res) {
+  try {
+    var doc = yaml.safeLoad(
+      fs.readFileSync('text/example.yml', 'utf8'));
+    res.render('index', {param:doc});
+  } catch (e) { console.log(e); }
 });
 
 app.use(express.static('public'));  // mount this directory to *web root*
 app.set('view engine', 'jade');     // use *Jade* to render templates
 app.set('views', './views');        // and here is the templates
 
-var server = app.listen(3000, function () { // start service
+var server = app.listen(3000, function () {
   console.log('Listening at port %s', server.address().port);
 });
