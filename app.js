7a45ebc1e842a454f0e94b1b68d6c87cc211bf47
LionNatsu <lionxlion@foxmail.com>
2015-06-15 01:38:00 +0800

    Wow here's Markdown\!
    Signed-off-by: LionNatsu <lionxlion@foxmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var yaml    = require('js-yaml');
 var fs      = require('fs');
+var md      = require( "markdown" ).markdown;
 var app     = express();
 
 app.get('/', function (req, res) {
   try {
     var doc = yaml.safeLoad(
       fs.readFileSync('text/example.yml', 'utf8'));
+    for(var i_ct in doc.contents) {
+      var content = doc.contents[i_ct].content;
+      for(var i_para in content)
+        content[i_para] = md.toHTML(content[i_para]);
+    }
     res.render('index', {param:doc});
   } catch (e) { console.log(e); }
 });
