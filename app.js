991c6b4eac4484f2986fd76c6f6ab6018622b0c5
LionNatsu <lionxlion@foxmail.com>
2015-06-20 21:33:16 +0800

    Beautiful structure
    Signed-off-by: LionNatsu <lionxlion@foxmail.com>

--- app.js
+++ app.js
@@ -4,22 +4,39 @@ var fs      = require('fs');
 var md      = require( "markdown" ).markdown;
 var app     = express();
 
-app.get('/', function (req, res) {
+function readYAML(yamlfile) {
   try {
-    var doc = yaml.safeLoad(
-      fs.readFileSync('text/example.yml', 'utf8'));
-    for(var i_ct in doc.contents) {
-      var content = doc.contents[i_ct].content;
-      for(var i_para in content)
-        content[i_para] = md.toHTML(content[i_para]);
-    }
-    res.render('index', {param:doc});
+    return yaml.safeLoad(fs.readFileSync('text/' + yamlfile + '.yml', 'utf8'));
   } catch (e) { console.log(e); }
-});
+}
 
-app.use(express.static('public'));  // mount this directory to *web root*
+function router(req, res) {
+  try {
+    switch(req.params.resource) {
+      case undefined :
+      case 'index.html' :
+        var doc = readYAML('news');
+        for(var i_ct in doc) {
+          var _ct = doc[i_ct].content;
+          for(var i_para in _ct) _ct[i_para] = md.toHTML(_ct[i_para]);
+        }
+        res.render('index', {'param':doc});
+        break;
+      case 'distro.html' :
+        var doc = readYAML('distro');
+        res.render('distro', {'param':doc});
+        break;
+      default:
+        res.status(404).render('err/404');
+    }
+  } catch (e) { console.log(e); res.status(505).render('err/505'); }
+}
+
 app.set('view engine', 'jade');     // use *Jade* to render templates
 app.set('views', './views');        // and here is the templates
+app.use(express.static('public'));  // mount this directory to *web root*
+app.get('/', router);
+app.get('/:resource', router);
 
 var server = app.listen(3000, function () {
   console.log('Listening at port %s', server.address().port);
