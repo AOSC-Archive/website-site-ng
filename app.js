b2abcc1e5823fa5b1226b1d41ebe34bac7d97bd0
LionNatsu <lionxlion@foxmail.com>
2015-07-03 10:37:40 +0800

    Jade won HTML
    Signed-off-by: LionNatsu <lionxlion@foxmail.com>

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var fs      = require('fs');
 var md      = require('markdown').markdown;
 var app     = express();
 
+function formatDate(date) {
+  var month = ['January', 'February', 'March', 'April', 'May',
+    'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return month[date.getUTCMonth()] + ' '
+               + date.getUTCDate() + ', '
+               + date.getUTCFullYear();
+}
+
 function readYAML(yamlfile) {
   try {
     return yaml.safeLoad(fs.readFileSync('text/' + yamlfile + '.yml', 'utf8'));
@@ -18,11 +26,13 @@ function router(req, res) {
         var bc = readYAML('news');
         var pj = readYAML('projects');
         for(var i_ct in bc) {
+          bc[i_ct].date = formatDate(bc[i_ct].date).toUpperCase();
           var _ct = bc[i_ct].content;
           for(var i_para in _ct) _ct[i_para] = md.toHTML(_ct[i_para]);
         }
         res.render('index',
-          {'param' : {
+          {'params' : {
+              'title' : 'Community Portal - AOSC',
               'broadcast' : bc,
               'projects' : pj
             }
---
+++ node_modules/jstransformer-markdown/HISTORY.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## v1.1.0: 2015-05-28
+
+- Update dependencies
+
+## v1.0.1: 2015-03-01
+
+- Add inputFormats
+
+## v1.0.0: 2015-02-28
+
+- Initial release
