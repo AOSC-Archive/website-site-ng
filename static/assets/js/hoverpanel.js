f930f52a1970cf182474c4f685a8e7200199cf85
LionNatsu <lionxlion@foxmail.com>
2015-09-02 16:23:47 +0800

    side panel: hover panel impl.

---
+++ static/assets/js/hoverpanel.js
@@ -0,0 +1,32 @@
+/*
+ * I am a plugin to make a panel slideDown when mouse hovers on it.
+ *
+ */
+(function($) {
+  $.fn.hoverPanel = function(options) {
+    var defaults = {
+      selector  : "p",
+      speed     : "fast",
+      delay     : 100,
+    }
+    return $(this).each(function() {
+
+      var setttings = $.extend(defaults, options || {});
+      var inTimer, outTimer;
+      var caller = $(this).find(setttings.selector);
+
+      $(this).hover(function() {
+        clearTimeout(outTimer);
+        inTimer = setTimeout(function() {
+          caller.slideDown(setttings.speed);
+        }, setttings.delay);
+      }, function() {
+        clearTimeout(inTimer);
+        outTimer = setTimeout(function() {
+          caller.slideUp(setttings.speed);
+        }, setttings.delay);
+      });
+
+    });
+  };
+})(jQuery);
