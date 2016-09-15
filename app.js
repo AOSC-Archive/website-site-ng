7ac59e5e02994cefefc1ec68f8fd1fe3a14e0776
Lion Yang <lion@aosc.xyz>
2016-09-15 15:53:27 +0800

    misc: clean app.js; new codename "Cauldron"

--- app.js
+++ app.js
@@ -1,53 +1,18 @@
-// ** DO **
-var express = require('express'); // ← Familiar things
+var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
-// Because it's still the **BACKEND**
-"Neop's love." // you love
-/* only better */
 
-
-// Do blink-of-an-WINK
-var app = express(); // ← things
+var app = express();
 var log = require('./controllers/log.js');
-// With quick start up and performance.
 
-// Do the same things
-// Because
-// your stuff
-// comes with you.
-
-/* Do personal things
-       private
-      whatever        */
-", such as sleeping together,"
-// With a more secure Framework.
-
-// Do
-// to-do things
-'"Plz read the fine manual" says the Jeff'
 app.use(bodyParser.urlencoded({ extended: false }));
 var crypto  = require('crypto');
 const COOKIE_SIGN_KEY_LENGTH = 256;
 app.use(cookieParser(crypto.randomBytes(COOKIE_SIGN_KEY_LENGTH).toString('binary')));
-app.set('view engine', 'pug'); // Use Lion and Pug (and Jade)
-app.set('views', './views');    // your truly personal digital
+app.set('view engine', 'pug');
+app.set('views', './views');
 app.set('trust proxy', 'loopback');
-// assistant
-
-// on your home
-// Reminding you to get
-{{};;{};['',"w"];}; // flowers
-
-// and
-// finding that static pages
-app.use(express.static('static')); // on your *public*
-
-// Do unexpected things
-// that turn browsing
-/* into doing */
-
-// Cha-cha-cha
+app.use(express.static('static'));
 app.use('/', require('./controllers/router.js'));
 app.use('/admin', require('./controllers/router-admin.js'));
 app.get( '*' , function(req, res) {
@@ -64,31 +29,14 @@ app.all( '*' , function(req, res) {
 var child_process = require('child_process');
 child_process.fork('./controllers/watcher.js');
 log.debug('server: Fork watcher');
-// tweet-tweet
-
-// With a one-stop AOSC Web Team
-/*  full of
-    straights,
-    lions,
-    and bis.  */
-
-// Do visual things
 var server = app.listen(3000, function () {
   log.debug('server: Listening at port ' + server.address().port);
 });
-// Like using AOSC OS for
-// Free
-// Zero
-// Zilch
-// Nada
-///////  FREE  ////////
-// Available in 2015 //
-/* Reserve a lion today. */
 
 /*
 
               AOSC
-          Web Team 2015
-             "Safe"
+          Web Team 2016
+           "Cauldron"
 
                               */
