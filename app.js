52afca1de78684d3f0f34863904d24f23c40a0d4
Lion Yang <lion@aosc.xyz>
2016-09-15 18:33:44 +0800

    app.js: comments

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser')
+const express = require('express');
+const log = require('./controllers/log.js');
 
-var app = express();
-var log = require('./controllers/log.js');
+// Express - Create The Instance
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-var crypto  = require('crypto');
-const COOKIE_SIGN_KEY_LENGTH = 256;
-app.use(cookieParser(crypto.randomBytes(COOKIE_SIGN_KEY_LENGTH).toString('binary')));
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.set('trust proxy', 'loopback');
-app.use(express.static('static'));
-app.use('/', require('./controllers/router.js'));
-app.use('/admin', require('./controllers/router-admin.js'));
-app.get( '*' , function(req, res) {
-  log.debug('router: Client requested a unreachable URI ' + req.originalUrl);
-  res.status(404).render('err/404', {'params' : {
-    'url' : req.path
-  }});
-});
-app.all( '*' , function(req, res) {
-  log.debug('router: Bad Request ' + req.originalUrl);
-  res.sendStatus(400);
-});
+// Parsing - Middlewares
+  app.use(require('body-parser').urlencoded({ extended: false }));
+  const crypto  = require('crypto');
+  const COOKIE_SIGN_KEY_LEN = 256;
+  const COOKIE_SIGN_KEY = crypto.randomBytes(COOKIE_SIGN_KEY_LEN).toString('binary');
+  app.use(require('cookie-parser')(COOKIE_SIGN_KEY));
 
-var child_process = require('child_process');
-child_process.fork('./controllers/watcher.js');
-log.debug('server: Fork watcher');
-var server = app.listen(3000, function () {
-  log.debug('server: Listening at port ' + server.address().port);
-});
+// Rendering - Engine
+  app.set('view engine', 'pug');
+  app.set('views', './views');
+
+// Routing - Filters
+  app.use(express.static('static'));
+  app.use('/', require('./controllers/router.js'));
+  app.use('/admin', require('./controllers/router-admin.js'));
+  app.get( '*' , (req, res) => {
+    log.debug('router: Client requested a unreachable URI ' + req.originalUrl);
+    res.status(404).render('err/404', {'params' : {
+      'url' : req.path
+    }});
+  });
+  app.all( '*' , (req, res) => {
+    log.debug('router: Bad Request ' + req.originalUrl);
+    res.sendStatus(400);
+  });
+
+// Network
+  app.set('trust proxy', 'loopback');
+  const server = app.listen(3000, function () {
+    log.debug('server: Listening at port ' + server.address().port);
+  });
+
+// Watcher - Stylus Compiler
+  require('child_process').fork('./controllers/watcher.js');
+  log.debug('server: Fork watcher');
 
 /*
 
