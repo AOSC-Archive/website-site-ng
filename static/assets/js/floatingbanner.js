f43a986562c6560b228bd945cb14c1e644457f63
Lion Yang <lion@aosc.xyz>
2016-09-20 19:57:18 +0800

    floatingbanner: performance

--- static/assets/js/floatingbanner.js
+++ static/assets/js/floatingbanner.js
@@ -6,21 +6,27 @@ $(function() {
     floatBannerStatus = false;
     $(".afe-floating-banner").hide();
 
+    var nav = $("#nav");
+    var banner = $(".afe-banner");
+    var staticBanner = $(".afe-static-banner");
+    var staticBannerContainer = $(".afe-static-banner>.container");
+    var floatingBanner = $(".afe-floating-banner");
+
     $(window).resize(function() {
-      navHeight = $("#nav").height();
-      floatBannerTop = $(".afe-banner").offset().top + $(".afe-static-banner").height() - $(".afe-floating-banner").height();
+      navHeight = nav.height();
+      floatBannerTop = banner.offset().top + staticBanner.height() - floatingBanner.height();
       $(document).scroll();
     }); $(window).resize();
 
     $(document).scroll(function() {
       if(!floatBannerStatus && $(document).scrollTop() + navHeight >  floatBannerTop) {
         floatBannerStatus = true;
-        $(".afe-static-banner>.container").addClass("invisible");
-        $(".afe-floating-banner").show().addClass("afe-fixed").css("top", navHeight + "px");
+        staticBannerContainer.addClass("invisible");
+        floatingBanner.show().addClass("afe-fixed").css("top", navHeight + "px");
       }else if(floatBannerStatus && $(document).scrollTop() + navHeight <= floatBannerTop) {
         floatBannerStatus = false;
-        $(".afe-static-banner>.container").removeClass("invisible");
-        $(".afe-floating-banner").hide().removeClass("afe-fixed").css("top", "auto");
+        staticBannerContainer.removeClass("invisible");
+        floatingBanner.hide().removeClass("afe-fixed").css("top", "auto");
       }
     });
   }
