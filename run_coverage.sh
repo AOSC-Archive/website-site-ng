a8c7ab7a1dc22d7f9f8ace334c56b953c3845c7c
liushuyu <liushuyu011@gmail.com>
2017-04-09 02:59:10 -0600

    Coverage: fix script

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+clean_up() {
+    kill -TERM "${redis_pid}";
+    exit "$1";
+}
+
 redis_pid=$(pidof redis-server)
 mocha_binary="./node_modules/mocha/bin/_mocha"
 if [[ ${redis_pid} -gt 0 ]]; then
@@ -10,9 +15,10 @@ else
   echo "Redis Server PID: $!"
 fi
 
-NODE_ENV=test ./node_modules/.bin/istanbul cover "${mocha_binary}" --report lcovonly -- -R spec && \
-		cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || true
-
+NODE_ENV=test ./node_modules/.bin/istanbul cover "${mocha_binary}" --report lcovonly -- -R spec
 mocha_status=$?
-kill -TERM ${redis_pid}
-exit ${mocha_status}
+if [[ ${mocha_status} -ne 0 ]]; then
+    clean_up ${mocha_status}
+fi
+./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info || true
+clean_up 0
